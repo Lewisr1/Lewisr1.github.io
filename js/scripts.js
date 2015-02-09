5616e3932dce99bed80c887016726d0d62a100ef
Paul Bacchus <paul@paulbacchus.com>
2015-02-09 16:07:11 +0000

    Updated carosel.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
-  $('.pics').slick({
+  $('.main-pics').slick({
     //dots: true,
     infinite: true,
     speed: 500,
     fade: true,
     cssEase: 'linear',
     arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+
+  $('.condensers').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
     autoplay: true,
     autoplaySpeed: 3000
   });
